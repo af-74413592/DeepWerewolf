a0e1753761857291a463230809dab8cc59f28fb6
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-10-14 14:13:36 +0800

    Create train-32b.sh

---
+++ examples/werewolf/train-32b.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+ulimit -n 1048576
+export N_GPUS=8
+export BASE_MODEL=/root/dataDisk/Qwen3-32B
+export DATA_DIR=/root/dataDisk/fake-data
+export ROLLOUT_TP_SIZE=8
+export EXPERIMENT_NAME=werewolf
+export PROJECT_NAME=AgentLightning
+export VLLM_FLASH_ATTN_VERSION=2
+export VLLM_USE_V1=1
+echo "Starting training script..."
+
+python -m agentlightning.verl \
+    algorithm.adv_estimator=grpo \
+    data.train_files=${DATA_DIR}/train.parquet \
+    data.val_files=${DATA_DIR}/test.parquet \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=8 \
+    trainer.n_gpus_per_node=${N_GPUS} \
+    data.train_batch_size=1 \
+    actor_rollout_ref.rollout.n=1 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.multi_turn.format=hermes \
+    actor_rollout_ref.model.path=${BASE_MODEL} \
+    data.max_prompt_length=14336 \
+    data.max_response_length=2048 \
+    data.truncation='middle' \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.kl_loss_coef=0.000 \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.clip_ratio_low=0.2 \
+    actor_rollout_ref.actor.clip_ratio_high=0.3 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.use_kl_in_reward=False \
+    trainer.default_local_dir='/root/dataDisk/checkpoints' \
+    trainer.max_actor_ckpt_to_keep=3 \
+    trainer.rollout_data_dir='/root/dataDisk/rollout' \
+    trainer.resume_mode=auto \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','tensorboard'] \
+    trainer.project_name=${PROJECT_NAME} \
+    trainer.experiment_name=${EXPERIMENT_NAME} \
+    trainer.nnodes=1 \
+    trainer.save_freq=1 \
+    trainer.test_freq=0 \
+    trainer.total_epochs=1 $@
