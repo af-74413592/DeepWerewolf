0f77770cab4e3bfea85aa029dd8cc3c1849da9d3
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-10-14 11:43:09 +0800

    Update train.sh

--- examples/werewolf/train.sh
+++ examples/werewolf/train.sh
@@ -26,7 +26,6 @@ python -m agentlightning.verl \
     data.max_prompt_length=14336 \
     data.max_response_length=2048 \
     data.truncation='middle' \
-    trainer.val_before_train=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.use_kl_loss=False \
