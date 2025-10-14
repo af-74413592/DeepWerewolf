d46e208a2c4e2184119acee9fd03003c81604146
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-10-14 11:44:42 +0800

    Update train-fsdp2.sh

--- examples/werewolf/train-fsdp2.sh
+++ examples/werewolf/train-fsdp2.sh
@@ -29,7 +29,6 @@ python -m agentlightning.verl \
     data.max_prompt_length=15360 \
     data.max_response_length=1024 \
     data.truncation='middle' \
-    trainer.val_before_train=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.ref.strategy=fsdp2 \
     actor_rollout_ref.actor.strategy=fsdp2 \
