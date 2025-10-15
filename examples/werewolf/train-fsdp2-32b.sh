beb460abc625b6c0e2edc6b011415903900ffa29
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-10-15 21:38:32 +0800

    Update train-fsdp2-32b.sh

--- examples/werewolf/train-fsdp2-32b.sh
+++ examples/werewolf/train-fsdp2-32b.sh
@@ -42,7 +42,7 @@ python -m agentlightning.verl \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.offload_policy=False \
+    actor_rollout_ref.actor.fsdp_config.offload_policy=True \
     algorithm.use_kl_in_reward=False \
     trainer.default_local_dir='/root/dataDisk/checkpoints' \
     trainer.max_actor_ckpt_to_keep=3 \
