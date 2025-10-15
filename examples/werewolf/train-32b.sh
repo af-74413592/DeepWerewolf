3a5dab4272168d016054e48599656a339c1a9958
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-10-15 13:53:02 +0800

    Update train-32b.sh

--- examples/werewolf/train-32b.sh
+++ examples/werewolf/train-32b.sh
@@ -37,7 +37,7 @@ python -m agentlightning.verl \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
